accdc02894abcadafe0f069ac1b49ad89341297f
rizkamimron <rizkamimron@gmail.com>
2024-07-18 21:48:34 +0700

    tes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,12 @@
 import { Home, Profile } from "@/pages";
+import { element } from "prop-types";
 
 export const routes = [
+  {
+    name: "main",
+    path: "/",
+    element: <Home/>,
+  },
   {
     name: "home",
     path: "/home",
